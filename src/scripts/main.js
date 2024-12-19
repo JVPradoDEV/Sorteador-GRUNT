5a113bb88f95cbf452d8e81e8ceb2a1d4943ad7c
bonnie <joaovitordoprado15@hotmail.com>
2024-12-19 13:58:13 -0300

    first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
+    console.log('EU EXISTOOOO');
     document.getElementById('form-sorteador').addEventListener('submit', function() {
-        let numeroMaximo = document.getElementsById('numero-maximo');
+        event.preventDefault();
+        let numeroMaximo = document.getElementById('numero-maximo').value;
         numeroMaximo = parseInt(numeroMaximo);
         
         let numeroAleatorio = Math.random() * numeroMaximo;
-        alert(numeroAleatorio);
+        numeroAleatorio = Math.floor(numeroAleatorio + 1);
+
+        document.getElementById('resultado-valor').innerText = numeroAleatorio;
+        document.querySelector(".resultado").style.display = 'block';
     })
 })
